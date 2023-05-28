c9cd33c235160deb25117d1f354ed58f397371d6
andrey pekarskiy <as@as.com>
2023-05-28 20:05:19 +0500

    next change

--- all_categories_dict.py
+++ all_categories_dict.py
@@ -1,17 +1,12 @@
-import random
-from time import sleep
 import requests
 from bs4 import BeautifulSoup
 import json
-import csv
-import lxml
 
 url = "https://www.cars.com/shopping/advanced-search/"
 headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
 }
-
 
 req = requests.get(url, headers=headers)
 src = req.text
@@ -25,7 +20,6 @@ for item in popular_makes:
     item_href = "https://www.cars.com/shopping/advanced-search/?list_price_max=&list_price_min=&makes[]=" + item['value'] + "&maximum_distance=all&mileage_max=&stock_type=all&year_max=&year_min=&zip="
     # print(f"{item_text}: {item_href}")
     all_categories_popular_dict[item_text] = item_href
-
 
 other_makes = soup.find(label = 'Other makes').find_all('option')
 all_categories_other_dict = {}
