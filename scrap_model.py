7dd38d1229281a98b96f94041b628de1055272dc
andrey pekarskiy <as@as.com>
2023-05-28 21:28:33 +0500

    pause

---
+++ scrap_model.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = "https://www.cars.com/shopping/advanced-search/"
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
+}
+
+
+with open("all_categories_dict.json") as file:
+    all_categories = json.load(file)
+
+# print(all_categories)
+count = 0
+for categroy_name, category_href in all_categories.items():
+    if count == 0:
+        req = requests.get(url = category_href, headers = headers)
+        src = req.text
+        soup = BeautifulSoup(src, "lxml")
+        popular_makes = soup.find_all(_class='child-group')
+        all_categories_popular_dict = {}
+        for item in popular_makes:
+            # print(item['value'])
+            item_text = item.text
+            print(item_text)
+
+            # item_href = "https://www.cars.com/shopping/advanced-search/?list_price_max=&list_price_min=&makes[]=" + \
+            #             item['value'] + "&maximum_distance=all&mileage_max=&stock_type=all&year_max=&year_min=&zip="
+            # print(f"{item_text}: {item_href}")
+            # all_categories_popular_dict[item_text] = item_href
+        count+=1
+
+# req = requests.get(url = "https://www.cars.com/shopping/advanced-search/?list_price_max=&list_price_min=&makes[]=acura&maximum_distance=all&mileage_max=&stock_type=all&year_max=&year_min=&zip=", headers = headers)
+# src = req.text
+# print(src)
