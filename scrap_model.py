d54022039fe086ca02db5e8d2abf5a644b879ecd
andrey pekarskiy <as@as.com>
2023-05-29 19:14:01 +0500

    scrap_model.py извлекает с лишними данными (количественное наличие автомобилей)

--- scrap_model.py
+++ scrap_model.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
 
 url = "https://www.cars.com/shopping/advanced-search/"
 headers = {
@@ -8,32 +9,16 @@ headers = {
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
 }
 
-
 with open("data/all_categories_dict.json") as file:
     all_categories = json.load(file)
 
-# print(all_categories)
 count = 0
 for categroy_name, category_href in all_categories.items():
-    if count <30:
+     if count <2:
         req = requests.get(url = category_href, headers = headers)
         src = req.text
-        # print(src)
-        soup = BeautifulSoup(src, "lxml")
-        popular_makes = soup.find('div', class_='child-group').find('div', class_='grey-category')
-        print(popular_makes)
-        # all_categories_popular_dict = {}
-        # for item in popular_makes:
-        #     # print(item['value'])
-        #     item_text = item.text
-        #     print(item_text)
-
-            # item_href = "https://www.cars.com/shopping/advanced-search/?list_price_max=&list_price_min=&makes[]=" + \
-            #             item['value'] + "&maximum_distance=all&mileage_max=&stock_type=all&year_max=&year_min=&zip="
-            # print(f"{item_text}: {item_href}")
-            # all_categories_popular_dict[item_text] = item_href
+        print(src)
+        soup = BeautifulSoup(src, 'lxml')
+        models = soup.find('div', {'id': 'mobile-models-wrapper'}).find('div', {'class': 'child-group'}).find_all('label', {'class': 'sds-label'})
+        print(models)
         count+=1
-
-# req = requests.get(url = "https://www.cars.com/shopping/advanced-search/?list_price_max=&list_price_min=&makes[]=acura&maximum_distance=all&mileage_max=&stock_type=all&year_max=&year_min=&zip=", headers = headers)
-# src = req.text
-# print(src)
