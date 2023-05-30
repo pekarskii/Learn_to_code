bff6aca76a296c7de12855951a8f9c97c467355f
Andrei Pekarskii <paguyc@gmail.com>
2023-05-30 18:41:50 +0500

    новая ветвь

--- 02_scrap_model.py
+++ 02_scrap_model.py
@@ -8,6 +8,7 @@ headers = {
     "Accept": "*/*",
     "User-Agent": "Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1"
 }
+
 with open("data/all_categories_dict.json") as file:
     all_categories = json.load(file)
 data = []
